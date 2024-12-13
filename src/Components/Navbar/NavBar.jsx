52c0366862b3b0d7295c8200ef3c11f6f1888523
DavidNabil210 <davidnabil633@gmail.com>
2024-12-13 23:05:56 +0200

    first commit

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,17 +1,22 @@
-import { BsFillMoonFillStars } from 'react-icons'
-import {AifillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillGithub,
+  AiFillFacebook,
+} from "react-icons/ai";
+
 function NavBar() {
 
   return (
 
     <>
       <main className='bg-white px-10'>
-        <section className="bg-blue-700 min-h-screen">
+        <section className="bg-blue-700 ">
           <nav className='py-10 mb-12 flex justify-between'>
             <h1 className='text-xl font-burtons'>hello</h1>
             <ul className='flex items-center'>
               <li>
-                <BsFillMoonFillStars className='cursor-pointer text-2xl' />
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
               </li>
               <li>
                 <a href="#" className='bg-gradient-to-r from-bg-cyan-500 to-teal-500 px-2 py-4 text-white'>Resume</a>
@@ -21,17 +26,19 @@ function NavBar() {
           </nav>
           <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium'>developer</h2>
-            <h3>dev</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            <h3 className='text-2xl py-2'>dev</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
               Voluptatem saepe earum ducimus tempora quisquam eos tenetur similique corrupti maiores dolorum?</p>
           </div>
-          <div>
-            <AiFillLinkedin/>
-            <AifillTwitterCircle/>
-            <AiFillYoutube/>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillGithub/>
+            <AiFillTwitterCircle/>
+            <AiFillFacebook/>
 
           </div>
-           
+           <div className=''>
+
+           </div>
         </section>
       </main>
     </>
